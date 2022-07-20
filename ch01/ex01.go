e7887b654b443182199270ad77213308e7b00035
Roman Likhachev <roman@itproblog.ru>
2022-07-20 16:25:07 +0700

    finish chapter 1

---
+++ ch01/ex01.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var originalCount int = 10
+	var eatenCount int = 4
+	fmt.Println("I start with", originalCount, "apples.")
+	fmt.Println("Some jerk ate", eatenCount, "apples.")
+	fmt.Println("There are", originalCount-eatenCount, "apples left")
+
+	var price int = 100
+	fmt.Println("Price is", price, "dollars.")
+	var taxRate float64 = 0.08
+	var tax float64 = float64(price) * taxRate
+	fmt.Println("Tax is", tax, "dollars.")
+	var total float64 = float64(price) + tax
+	fmt.Println("Total cost is", total, "dollars.")
+	var availableFunds int = 120
+	fmt.Println(availableFunds, "dollars available.")
+	fmt.Println("Within budget?", total <= float64(availableFunds))
+}
